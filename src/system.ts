bd596f41d765ab851fe8aa32750f85a705a24ef8
jiji262 <jiguofei@msn.com>
2023-03-23 20:58:12 +0800

    first commit

---
+++ src/system.ts
@@ -0,0 +1,17 @@
+export const setting = {
+  continuousDialogue: false,
+  archiveSession: true,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  password: "",
+  systemRule: ""
+}
+
+export const message = `- 由 [OpenAI API](https://platform.openai.com/docs/guides/chat) 提供支持。
+- [[Shift]] + [[Enter]] 换行，开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设，点击输入框滚动到底部。
+- 为了节省token消耗，默认不开启连续对话功能。
+- 当前没有给网站设置密码，请勿滥用。`
+
+export type Setting = typeof setting
+
+export const resetContinuousDialogue = true
